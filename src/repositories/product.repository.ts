27cc852bbf202a0d401365628e1a0149a7c50a2d
prabhakaran-code <prabhakaran.u@hubino.com>
2026-04-06 11:44:23 +0530

    redis-added

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,7 +1,34 @@
 import db from "../config/db";
 import { Product } from "../types/product.types";
 
+// ✅ GET
 export const getAllProducts = async (): Promise<Product[]> => {
   const [rows] = await db.query("SELECT id, name, price, category FROM products");
   return rows as Product[];
+};
+
+// ✅ CREATE
+export const createProduct = async (data: Product) => {
+  const { name, price, category } = data;
+
+  await db.query(
+    "INSERT INTO products (name, price, category) VALUES (?, ?, ?)",
+    [name, price, category]
+  );
+};
+
+// ✅ UPDATE
+export const updateProduct = async (id: number, data: Product) => {
+  const { name, price, category } = data;
+
+  await db.query(
+    "UPDATE products SET name=?, price=?, category=? WHERE id=?",
+    [name, price, category, id]
+  );
+};
+
+
+// ✅ DELETE
+export const deleteProduct = async (id: number) => {
+  await db.query("DELETE FROM products WHERE id=?", [id]);
 };
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -2,26 +2,52 @@ import redisClient from "../config/redis";
 import * as productRepo from "../repositories/product.repository";
 import { Product } from "../types/product.types";
 
-export const getProducts = async (): Promise<Product[]> => {
+export const getProducts = async () => {
   const cacheKey = "products";
 
-  // Check cache
   const cached = await redisClient.get(cacheKey);
 
   if (cached) {
-    console.log("Cache HIT");
+    console.log("🔥 Cache HIT");
     return JSON.parse(cached);
   }
 
-  // DB call
+  console.log("🐢 Cache MISS");
+
   const products = await productRepo.getAllProducts();
 
-  // Save cache
   await redisClient.set(cacheKey, JSON.stringify(products), {
     EX: 60,
   });
 
-  console.log("Cache MISS");
-
   return products;
+};
+
+
+export const createProduct = async (data: any) => {
+  await productRepo.createProduct(data);
+
+  // 🔥 Clear cache
+  await redisClient.del("products");
+
+  return { message: "Product created" };
+};
+
+
+export const updateProduct = async (id: number, data: any) => {
+  await productRepo.updateProduct(id, data);
+
+  // 🔥 Clear cache
+  await redisClient.del("products");
+
+  return { message: "Product updated" };
+};
+
+export const deleteProduct = async (id: number) => {
+  await productRepo.deleteProduct(id);
+
+  // 🔥 Invalidate cache
+  await redisClient.del("products");
+
+  return { message: "Product deleted" };
 };
